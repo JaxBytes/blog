a330557daf2e3e35d3b019abfce20b64e221c5a7
JaxBytes <JaxBytes@gmail.com>
2025-11-11 20:50:49 +0800

    修复置顶bug

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -134,9 +134,9 @@ onMount(async () => {
                      group-hover:translate-x-1 transition-all group-hover:text-[var(--primary)]
                      text-75 pr-8 whitespace-nowrap overflow-ellipsis overflow-hidden"
                         >
-                        {post.data.pinned &&
-                                        <Icon class="inline-block text-[1rem] text-[var(--primary)] mr-1 align-middle translate-y-[-1px]" name="material-symbols:push-pin" />
-                        }
+                            {#if post.data.pinned}
+                                <Icon class="inline-block text-[1rem] text-[var(--primary)] mr-1 align-middle translate-y-[-1px]" name="material-symbols:push-pin" />
+                            {/if}
                             {post.data.title}
                         </div>
 
